333dfa9b7e32642c732b6ce89dd8048aeccdf073
MariiaOtset <152022954+MariiaOtset@users.noreply.github.com>
2024-04-15 19:17:41 +0100

    done

--- src/js/02-form.js
+++ src/js/02-form.js
@@ -1,13 +1,12 @@
-// Використовуючи делегуваня, відстежуй на формі подію input і щоразу записуй у локальне сховище об'єкт з полями email і message, у яких зберігай поточні значення полів форми. Нехай ключем для сховища буде рядок "feedback-form-state".
 // Під час завантаження сторінки перевіряй стан сховища, і якщо там є збережені дані, то заповнюй ними поля форми. В іншому випадку поля повинні бути порожніми.
 // Під час сабміту форми очищай сховище і поля форми, а також виводь у консоль об'єкт з полями email, message та їхніми поточними значеннями.
 
 const form = document.querySelector(".feedback-form");
 
-form.addEventListener("input", inputForm);
+form.addEventListener("input", saveToLocaleStorage);
 form.addEventListener("submit", onSubmit);
 
-function inputForm(event) {
+function saveToLocaleStorage(event) {
     const formData = {
         email: event.currentTarget.elements.email.value.trim(),
         message: event.currentTarget.elements.message.value.trim()
@@ -15,15 +14,43 @@ function inputForm(event) {
     localStorage.setItem("feedback-form-state", JSON.stringify(formData));
 }
 
-function loadData() {
-     const savedData = JSON.parse(localStorage.getItem("feedback-form-state"));
-    if (savedData) {
-        email.value = savedData.email || '';
-        message.value = savedData.message || '';
-    }
-}
+function clearFormInputs() {
+  form.elements.email.value = '';
+  form.elements.message.value = '';
+};
+
+const setFormInputValues = (formRef, { email, message }) => {
+    formRef.elements.email.value = email  ?? '';
+  formRef.elements.message.value = message ?? '';
+};
 
 function onSubmit(event) {
-     event.preventDefault();
+    event.preventDefault();
+    
+     const { email, message } = event.currentTarget.elements;
+    if (!email.value && !message.value) {
+        return alert("Email and message inputs should be filled in")
+    };
 
-}
+    const data = {
+        email: event.currentTarget.elements.email.value.trim(),
+        message: event.currentTarget.elements.message.value.trim()
+    }
+    console.log(data)
+
+    clearFormInputs();
+    localStorage.removeItem("feedback-form-state");
+};
+
+function initializeFormData() {
+  try {
+    const formData =
+      JSON.parse(localStorage.getItem('feedback-form-state')) ?? {};
+
+    setFormInputValues(form, formData);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+initializeFormData();
